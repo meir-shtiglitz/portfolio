c4c8357ea8cbbe8e29386b5079e1e240a09b1abb
meir shtiglitz <m.stigel@gmail.com>
2022-11-11 10:34:16 +0200

    add fidback to contact sending

--- src/controlers+/sendMails.js
+++ src/controlers+/sendMails.js
@@ -1,10 +1,25 @@
 import axios from "axios"
+import Swal from "sweetalert2";
 
 export const SendMail = async(data) => {
-    let result = await axios.post(process.env.REACT_APP_API, data,
-    {headers:{
-        "Content-Type": "application/json",
-    }});
+    try {
+        let result = await axios.post(process.env.REACT_APP_API, data,
+        {headers:{
+            "Content-Type": "application/json",
+        }});
+        Swal.fire({
+            icon: 'success',
+            title: 'The message has been sent',
+            text: 'We will be in touch'
+        })
+    } catch (error) {
+        console.log('error in send mail', error)
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops... somthing went wrong',
+            text: 'Please try again later'
+        })
+    }
 }
 
 
